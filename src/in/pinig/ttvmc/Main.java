74aa1ab216ffa13f7c07ec5ba29ba3e54c3a06ae
Maksim Pinigin <pinigin@mapicom.ru>
2020-02-11 10:27:14 +0700

    Sending QUIT to IRC before plugin is unload

--- src/in/pinig/ttvmc/Main.java
+++ src/in/pinig/ttvmc/Main.java
@@ -1,10 +1,21 @@
 package in.pinig.ttvmc;
 
+import java.io.IOException;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(new EListener(), this);
+	}
+	
+	@Override
+	public void onDisable() {
+		try {
+			TMI.out.write("QUIT\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
